8a881a3a7a74ef8684c1949e0fb89cb47612dd74
Yarangnight <yarangnight@gmail.com>
2020-07-07 16:35:05 +0900

    chapterselect added

--- Assets/Script/TitlePress2Start.cs
+++ Assets/Script/TitlePress2Start.cs
@@ -6,6 +6,6 @@ public class TitlePress2Start : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("StageSelectScene");
+        SceneManager.LoadScene("ChapterSelect");
     }
 }
